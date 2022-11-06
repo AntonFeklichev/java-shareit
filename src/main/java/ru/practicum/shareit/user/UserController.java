ec3b0cdfc18d84eaa0af28a3f8ff78952dff7312
Anton Feklichev <afeklichev@yandex.ru>
2022-11-06 04:50:01 +0300

    feat: Add UserController

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,49 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
 @RequestMapping(path = "/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public UserDto addUser(@Valid @RequestBody
+                           UserDto userDto) {
+        return userService.addUser(userDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void removeUserById(@PathVariable Long userId) {
+        userService.removeUserById(userId);
+    }
+
+    @PatchMapping("/{userId}")
+    public UserDto updateUser(@Valid @RequestBody
+                              UserDto userDto,
+                              @PathVariable
+                              Long userId) {
+        userDto.setId(userId);
+        return userService.updateUser(userDto);
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto findById(@PathVariable Long userId) {
+        return userService.findById(userId);
+    }
+
+    @GetMapping
+    public List<UserDto> findAll() {
+        return userService.findAll();
+    }
 }
